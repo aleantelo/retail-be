1bed03c61e3266a1be819ad9e269e2f7c6b26a30
Alejandro Antelo <alejandronicolasantelo@gmail.com>
2022-01-27 17:51:22 -0300

    modificacion cors2

--- src/main/java/com/sofftek/demo/controller/ProductoController.java
+++ src/main/java/com/sofftek/demo/controller/ProductoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://retailsoff-fe.herokuapp.com/")
 @RestController
 public class ProductoController {
 
